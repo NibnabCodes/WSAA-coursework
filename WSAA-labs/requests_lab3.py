750913118d67d141e5b852663850c0de9605db17
Niamh Hogan <hniamhh1995@gmail.com>
2026-02-12 22:27:32 +0000

    fixed lab3 code - got most working

--- WSAA-labs/requests_lab3.py
+++ WSAA-labs/requests_lab3.py
@@ -1,22 +1,18 @@
 # Write the code to get the books from http://andrewbeatty1.pythonanywhere.com/books 
 import requests 
+import json 
 url = "http://andrewbeatty1.pythonanywhere.com/books"
-#response = requests.get(url) 
-#print (response.json())
+response = requests.get(url) 
 
 def getAllBooks():
     response = requests.get(url)
     return response.json()
-#print(getAllBooks())
 
 # Convert that into a function and call it from inside a if __name__ == “__main__”:
 def readbooks():
     response = requests.get(url)
     response.raise_for_status()  # checks for HTTP errors
     return response.json()
-
-if __name__ == "__main__":
-    print(readbooks()) 
     
 # Write the function for find by id and test it (you need to write the testing code) 
 def getBookById(id):
@@ -27,17 +23,12 @@ def getBookById(id):
     response.raise_for_status()
     return response.json()
 
-if __name__ == "__main__":
-    print(getBookById(1))
-    # The function is correct. A 500 error indicates a server-side issue, 
-    # so I handle it using exception handling rather than changing the client code. 
-    
 # write the code to create and test it (you need to write your own testing code) 
 def createbook(book):
     response = requests.post(url, json=book)
 
     # 201 = Created (expected for POST)
-    if response.status_code != 201:
+    if response.status_code != 200:
         raise Exception(
         f"Failed to create book "
         f"(Status: {response.status_code}) "
@@ -47,9 +38,9 @@ def createbook(book):
     return response.json()
 
 # Write the update function
-def updatebook(id, book):
-    puturl = f"{url}/{id}"
-    response = requests.put(puturl, json=book)
+def updateBook(id, bookdiff):
+    updateurl = url + "/" + str(id)
+    response = requests.patch(updateurl, json=bookdiff)
 
     if response.status_code != 200:
         raise Exception(
@@ -57,12 +48,11 @@ def updatebook(id, book):
             f"(Status: {response.status_code}) "
             f"Response: {response.text}"
         )
-
     return response.json()
 
 # Write the delete function 
 def deletebook(id): 
-    deleteurl = URL + "/" + str(id) 
+    deleteurl = url + "/" + str(id) 
     response = requests.delete(deleteurl)
     
     if response.status_code != 200:
@@ -75,4 +65,18 @@ def deletebook(id):
     return response.json() 
 
 if __name__ == "__main__":
-    print(deletebook(1552))
+    book= {
+        'author':"F. Scott Fitzgerald",
+        'title':"The Great Gatsby",
+        "price": 123
+    }
+    bookdiff= {
+        "price": 444
+    }
+    #print(response.json())
+    #print(getAllBooks())
+    #print(readbooks())
+    print(getBookById(1630))
+    #print(createbook(book))
+    print(updateBook(id, bookdiff))
+    #print(deletebook(1625))
