81a49051fae8cb40ca5df2162bc82a167ca9a287
Niamh Hogan <hniamhh1995@gmail.com>
2026-02-26 19:49:12 +0000

    converted webpage to pdf with API key

---
+++ WSAA-labs/config4.py
@@ -0,0 +1,4 @@
+apikeys = {
+    "htmltopdfkey":"XTc52I51Q2CsnwnPpmTw9cwugbHCGF6O862bMY4bIUUMVUnZgC4T71C5g7AX29mM"
+}
+
---
+++ WSAA-labs/lab4.2.API_keys.py
@@ -0,0 +1,26 @@
+# This program converts a web page to a pdf 
+# to demonstrate API Keys
+# Author: Niamh Hogan
+
+import requests
+import urllib.parse
+from config4 import apikeys as cfg
+
+targetUrl = "https://andrewbeatty1.pythonanywhere.com/bookviewer.html" 
+ 
+apikey = cfg["htmltopdfkey"]
+apiurl = 'https://api.html2pdf.app/v1/generate'
+
+params = {'html': targetUrl,'apiKey': apikey}
+parsedparams = urllib.parse.urlencode(params)
+
+requestUrl = apiurl +"?" + parsedparams 
+print (requestUrl)
+
+response = requests.get(requestUrl)
+
+print (response.status_code)
+result =response.content
+
+with open("lab4document.pdf", "wb") as handler:
+    handler.write(result) 
