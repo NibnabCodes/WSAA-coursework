ee3472f3d76c4ff75a652b6875256efe9069be4b
Niamh Hogan <hniamhh1995@gmail.com>
2026-02-27 14:49:29 +0000

    added test.txt file & appending text to pfda_assignments

--- WSAA-labs/lab4.3_ApiGit.py
+++ WSAA-labs/lab4.3_ApiGit.py
@@ -1,9 +1,40 @@
 #
 
 from github import Github 
-from config import config as cfg 
+#import sys
+#sys.path.append("..") 
+from config4 import apikeys as cfg 
+import requests
 
 
-apikey = cfg["githubkey"] 
-# use your own key 
+apikey = cfg["pfda_assignments"] 
 g = Github(apikey) 
+
+# Checking that PyGithub works and that the key is valid by printing the name of the authenticated user
+# & repositories of the authenticated user
+#print(g.get_user().name)
+#for repo in g.get_user().get_repos(): 
+	#print(repo.name) 
+
+# Getting a specific repository by name and printing its clone URL
+repo = g.get_repo("NibnabCodes/pfda_assignments") 
+#print(repo.clone_url) 
+
+# Getting specific file in the repository and printing its download URL
+fileInfo = repo.get_contents("test.txt") 
+urlOfFile = fileInfo.download_url 
+#print (urlOfFile) 
+
+# Downloading the content of the file using the download URL and printing it
+response = requests.get(urlOfFile) 
+contentOfFile = response.text 
+#print (contentOfFile) 
+
+# Appending some text to the content of the file and printing the new content
+newContents = contentOfFile + " more stuff \n" 
+#print(newContents) 
+
+# Updating the file in the repository with the new content and printing the response from GitHub
+gitHubResponse=repo.update_file(fileInfo.path,"updated by prog",
+newContents,fileInfo.sha) 
+print (gitHubResponse) 
