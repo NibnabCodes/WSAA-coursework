67274149be86acce33e197b8f5ac703616a52131
Niamh Hogan <hniamhh1995@gmail.com>
2026-03-09 21:03:49 +0000

    redid restful api lab

--- WSAA-labs/rest-server.py
+++ WSAA-labs/rest-server.py
@@ -1,51 +1,78 @@
 # simple flask server
 
-from flask import Flask, request
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+# Simple in-memory "database"
+books = [
+    {"id": 1, "title": "Brave New World", "author": "Aldous Huxley", "price": 5},
+    {"id": 2, "title": "Crime and Punishment", "author": "Fyodor Dostoevsky", "price": 6}
+]
+
+# basic test endpoint to confirm the server works
+# http://127.0.0.1:5000/
 @app.route('/')
 def index():
-        return "Hello there"
+        return "Book API is running"
 
-# getall
+# Get all books
 # curl http://127.0.0.1:5000/books
-
 @app.route('/books', methods=['GET'])
 def getall():
-        return "get all"
+    return jsonify(books)
 
-# find by id
+
+# Find by id
 # curl http://127.0.0.1:5000/books/1
-
 @app.route('/books/<int:id>', methods=['GET'])
 def findbyid(id):
-        return "find by id"
+    for book in books:
+        if book["id"] == id:
+            return jsonify(book)
+    return jsonify({"error": "Book not found"}), 404
 
-#create
-#curl -X POST -d "{\"title\":\"test\", \"author\":\"some guy\", \"price\":123}" http://127.0.0.1:5000/books
+# Create Book
+# curl -X POST -H "Content-Type: application/json" -d "{\"title\":\"Animal Farm\",\"author\":\"George Orwell\",\"price\":10}" http://127.0.0.1:5000/books
 @app.route('/books', methods=['POST'])
 def create():
-        # read json from the body
-        jsonstring = request.json
+    newbook = request.json
 
-        return f"create {jsonstring}"
+    # create new id
+    newbook["id"] = books[-1]["id"] + 1 if books else 1
+ 
+    books.append(newbook)
 
-# update
+    return jsonify(newbook), 201
+
+# update book by id
 # curl -X PUT -d "{\"title\":\"test\", \"author\":\"some guy\", \"price\":123}" http://127.0.0.1:5000/books/1
 
 @app.route('/books/<int:id>', methods=['PUT'])
 def update(id):
-        jsonstring = request.json
-        return f"update {id} {jsonstring}"
+    updatedbook = request.json
 
-# Delete
+    for book in books:
+        if book["id"] == id:
+            book["title"] = updatedbook.get("title", book["title"])
+            book["author"] = updatedbook.get("author", book["author"])
+            book["price"] = updatedbook.get("price", book["price"])
+
+            return jsonify(book)
+
+    return jsonify({"error": "Book not found"}), 404
+
+# Delete Book by id
 # curl -X DELETE  http://127.0.0.1:5000/books/1
 
 @app.route('/books/<int:id>', methods=['DELETE'])
 def delete(id):
-        return f"delete {id}"
+    for book in books:
+        if book["id"] == id:
+            books.remove(book)
+            return jsonify({"message": "Book deleted"})
 
+    return jsonify({"error": "Book not found"}), 404
 
 if __name__ == "__main__":
     app.run(debug = True)
